34bc246afbc8114751158504c1bb4e9086390819
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-13 19:34:23 +0300

    Add fix for hook useFetchData

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,9 +2,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { purple } from '@mui/material/colors';
 
 export default function CustomAppBar() {
@@ -12,23 +9,13 @@ export default function CustomAppBar() {
         <Box as={'header'} sx={{ flexGrow: 1 }}>
             <AppBar position="static" sx={{ bgcolor: purple[500] }}>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography
                         variant="h6"
                         component="div"
                         sx={{ flexGrow: 1 }}
                     >
-                        News
+                        NASA APIs
                     </Typography>
-                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </Box>
