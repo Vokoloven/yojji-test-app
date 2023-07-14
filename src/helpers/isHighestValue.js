209b7edb6ad058b1cff52947c3023f4b0f3eee81
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-14 15:42:28 +0300

    Fixed is_potentially_hazardous_asteroid === 0

--- src/helpers/isHighestValue.js
+++ src/helpers/isHighestValue.js
@@ -1,5 +1,7 @@
 export const isHighestValue = (sorted, id) => {
-    const isHighestValue = sorted.some((item) => item.id === id);
+    const isHighestValue = sorted.some(
+        (item) => item.is_potentially_hazardous_asteroid !== 0 && item.id === id
+    );
 
     return isHighestValue;
 };
